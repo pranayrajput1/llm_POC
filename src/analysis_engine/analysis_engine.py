66afcd604ea6bcec604d00e33c90653d87865957
Sans1509 <sanskriti.tiwari@knoldus.com>
2023-07-24 17:38:42 +0530

    latest changes

--- src/analysis_engine/analysis_engine.py
+++ src/analysis_engine/analysis_engine.py
@@ -1,6 +1,7 @@
 from src.utils.helpers.entries_fetcher import get_entries
 from src.utils.helpers.input_helpers import get_user_query, get_keywords
 from src.utils.helpers.log_setup import get_log
+from src.utils.helpers.select_function import extract_operation, select_function_based_on_keyword
 
 # getting log setup
 logging = get_log()
@@ -13,6 +14,8 @@ class AnalysisEngine:
         user_query = get_user_query()
         extracted_keywords = get_keywords(user_query)
         response = get_entries(extracted_keywords)
+        operation = extract_operation(user_query)
+        function = select_function_based_on_keyword(response,operation)
         return response
 
 
@@ -20,3 +23,4 @@ if __name__ == "__main__":
     analysis_instance = AnalysisEngine()
     result = analysis_instance.analysis_pipeline()
     print(result.items())
+    print(len(result))
