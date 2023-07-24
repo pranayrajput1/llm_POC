9d0e3fd055572df640fbdade45e38bb9c8f63709
Sans1509 <sanskriti.tiwari@knoldus.com>
2023-07-24 18:39:26 +0530

    latest changes

--- src/analysis_functions/max_values.py
+++ src/analysis_functions/max_values.py
@@ -1,7 +1,11 @@
 def get_individual_max_values(data_entries):
-    items = data_entries.values
-    max_value = items.max()
-    return max_value
+    keys_list = list(data_entries.keys())
+
+    max_values = []
+    for item in keys_list:
+        print(item)
+        max_values.append(max(data_entries[item]))
+    return max_values
 
 
 def get_max_between_multiple(data_entries):
