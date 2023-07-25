efc811d8945e2ad3485a4c7bcf56fee84c8d5bdd
Sans1509 <sanskriti.tiwari@knoldus.com>
2023-07-25 18:51:56 +0530

    Implemented prompt generation

--- src/analysis_engine/analysis_engine.py
+++ src/analysis_engine/analysis_engine.py
@@ -1,25 +1,47 @@
-from src.utils.helpers.entries_fetcher import get_entries
-from src.utils.helpers.input_helpers import get_user_query, get_keywords
-from src.utils.helpers.log_setup import get_log
-from src.utils.helpers.select_function import extract_operation, select_function_based_on_keyword
+import pandas as pd
+from src.utils.helpers.input_helpers import get_user_query
+from src.utils.helpers.select_function import extract_operation, select_function_based_on_keyword, column_name, get_log
 
-# getting log setup
 logging = get_log()
+df = pd.DataFrame(columns=["Question", "Answer"])
 
 
 class AnalysisEngine:
 
     @staticmethod
     def analysis_pipeline():
+        """
+        calling all the function for the pipeline and returning output
+        :return function
+        """
         user_query = get_user_query()
-        extracted_keywords = get_keywords(user_query)
-        response = get_entries(extracted_keywords)
         operation = extract_operation(user_query)
-        function = select_function_based_on_keyword(response,operation)
-        return function
+        extracted_column_name = column_name(user_query)
+        if extracted_column_name is not None:
+            platform1 = extracted_column_name[0]
+            platform2 = extracted_column_name[1] if len(extracted_column_name) > 1 else None
+
+            function = select_function_based_on_keyword(user_query,operation, platform1, platform2)
+            return function
+        else:
+            return "No matching column names found in the input query."
 
 
 if __name__ == "__main__":
     analysis_instance = AnalysisEngine()
-    result = analysis_instance.analysis_pipeline()
-    print(result)
+    while True:
+        question, answer = analysis_instance.analysis_pipeline()
+
+        df = pd.concat([df, pd.DataFrame({"Question": [question], "Answer": [answer]})], ignore_index=True)
+
+        print("\nUpdated Dataframe:")
+        print(df)
+
+        choice = input("Do you want to continue? (y/n): ")
+        if choice.lower() != "y":
+            df.to_csv("question_answer_dataset.csv", index=False)
+            print("Dataframe saved to 'question_answer_dataset.csv'")
+            print("Goodbye!")
+            break
+
+
